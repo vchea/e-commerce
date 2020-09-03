560c194b5e06a860ede91b0a7015bd2044f11bc6
Vimean Chea <vimeanc@csu.fullerton.edu>
2020-09-03 14:06:20 -0700

    add signin

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './sign-up.styles.scss';
+
+class signUp extends React.Component(){
+    construstor(props){
+        super(props);
+        this.state ={
+            firstName='',
+            lastName ='',
+            email ='',
+            password=''
+        }
+    }
+
+    render(){
+        <div className = 'sign-up'> 
+        </div>
+    }
+ 
+
+}
